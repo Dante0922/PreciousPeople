18485ed883ef49d909023bb1cb78e9ce9e137d24
gw8413 <gw8413@gmail.com>
2023-06-11 20:43:40 +0900

    Relationship entity 고민 중...

---
+++ lib/features/relationship/models/relationship_model.dart
@@ -0,0 +1,15 @@
+class RelationshipModel{
+  String friendId;
+  String startDate;
+  String endDate;
+  String period;
+
+
+  RelationshipModel({
+    required this.friendId,
+    required this.startDate,
+    required this.endDate,
+    required this.period,
+  });
+
+}
